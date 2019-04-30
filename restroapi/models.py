17ad67ff5cadfc72d371137f73adc58ddc4f1623
Global Digital <nabinwell@gmail.com>
2019-04-30 17:09:59 +0545

    model

--- restroapi/models.py
+++ restroapi/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Restro(models.Model):
-    created = models.DateTimeField(auto_now_add=True)
+    
     name = models.CharField(max_length = 255, verbose_name = u'Name')
     email = models.CharField(max_length = 255, verbose_name = u'Email')
     logo = models.CharField(max_length = 255, verbose_name = u'Logo')
